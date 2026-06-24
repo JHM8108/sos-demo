e0c4948f0e5812bd1ae12df6814623aa0bf04ee6
jhm8108 <366238108@qq.com>
2026-06-24 19:24:06 +0800

    Fix underscore filenames for GitHub Pages Jekyll

--- assets/uni-app.es.EQscg2fu.js
+++
@@ -1,1 +0,0 @@
-import{U as s,V as a,W as o,X as r}from"./index-BJ_Lkd0e.js";const t=(o=>(t,e=a())=>{!r&&s(o,t,e)})(o,3);export{t as o};
---
+++ assets/uni-app.es.XEOx-DWi.js
@@ -0,0 +1,1 @@
+import{U as s,V as a,W as o,X as r}from"./index-0xit3vzU.js";const t=(o=>(t,e=a())=>{!r&&s(o,t,e)})(o,3);export{t as o};
